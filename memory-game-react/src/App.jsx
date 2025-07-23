c7e65602870ae7b212a4ed9430c0dfd61dc3fdee
Lazar Vuckovic <lazarvuckovic777@gmail.com>
2025-07-23 17:57:05 +0200

    Finished...

--- memory-game-react/src/App.jsx
+++ memory-game-react/src/App.jsx
@@ -3,10 +3,61 @@ import "./index.css";
 import Li from "./Li.jsx";
 import productsData from "./data";
 
+function shuffleArray(array) {
+  const shuffled = [...array];
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+  return shuffled;
+}
+
 function App() {
   const [currentScore, setCurrentScore] = useState(0);
   const [highScore, setHighScore] = useState(0);
   const [characters, setCharacters] = useState(productsData);
+  const [clickedItems, setClickedItems] = useState([]);
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  const playAgain = () => {
+    toggleModal();
+    setClickedItems([]);
+    setCurrentScore(0);
+  };
+
+  const increaseScore = () => {
+    setCurrentScore((prevScore) => {
+      const newScore = prevScore + 1;
+
+      if (newScore > highScore) {
+        setHighScore(newScore);
+      }
+
+      if (newScore === 12) {
+        toggleModal();
+      }
+
+      return newScore;
+    });
+  };
+
+  const handleClick = (character) => {
+    if (clickedItems.includes(character.id)) {
+      setClickedItems([]);
+      toggleModal();
+    } else {
+      console.log(character.name, true);
+      setClickedItems((prev) => [...prev, character.id]);
+      increaseScore();
+    }
+
+    const newCharacters = shuffleArray(characters);
+    setCharacters(newCharacters);
+  };
 
   return (
     <>
@@ -24,9 +75,23 @@ function App() {
             id={character.id}
             name={character.name}
             img={character.image}
+            onClick={() => handleClick(character)}
           />
         ))}
       </ul>
+      {modal && (
+        <div className="modal" onClick={playAgain}>
+          <div className="overlay">
+            <div className="modal-content">
+              <h1 className="modal-h1">Game over!</h1>
+              <p className="modal-p">Your score: {currentScore}</p>
+              <button className="play-btn" onClick={playAgain}>
+                Play again?
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
       <footer className="footer">
         <p className="footer-paragraph">Copyright © 2025 Lazar Vuckovic</p>
       </footer>
--- memory-game-react/src/Li.jsx
+++ memory-game-react/src/Li.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 
-function Li({ img, name }) {
+function Li({ img, name, onClick }) {
   return (
     <li>
-      <img src={img} alt={name} className="character-img" />
+      <img onClick={onClick} src={img} alt={name} className="character-img" />
       <h3 className="character-name">{name}</h3>
     </li>
   );
