fcc90305ce82494b6693693ffe5633b7b2f30d36
Lazar Vuckovic <lazarvuckovic777@gmail.com>
2025-07-24 16:32:53 +0200

    ...

--- memory-game-react/src/App.jsx
+++ memory-game-react/src/App.jsx
@@ -4,12 +4,7 @@ import Li from "./Li.jsx";
 import productsData from "./data";
 
 function shuffleArray(array) {
-  const shuffled = [...array];
-  for (let i = shuffled.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-  }
-  return shuffled;
+  return [...array].sort(() => Math.random() - 0.5);
 }
 
 function App() {
